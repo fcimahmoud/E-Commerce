8a338883b0faaa574e5d559913d9b99ab3da2b04
blyatsho <ma5740@fayoum.edu.eg>
2024-09-29 18:04:34 +0100

    Generic Repositoriy

---
+++ Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace Persistence.Repositories
+{
+    internal class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey>
+        where TEntity : BaseEntity<TKey>
+    {
+        private readonly StoreContext _storeContext;
+        public GenericRepository(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        public async Task AddAsync(TEntity entity) => await _storeContext.Set<TEntity>().AddAsync(entity);
+        public void Delete(TEntity entity) => _storeContext.Set<TEntity>().Remove(entity);
+        public void Update(TEntity entity) => _storeContext.Set<TEntity>().Update(entity);
+        public async Task<TEntity?> GetAsync(TKey id) => await _storeContext.Set<TEntity>().FindAsync(id);
+        public async Task<IEnumerable<TEntity?>> GetAllAsync(bool trackChanges) => trackChanges ? await _storeContext.Set<TEntity>().ToListAsync() : await _storeContext.Set<TEntity>().AsNoTracking().ToListAsync();
+
+
+    }
+}
