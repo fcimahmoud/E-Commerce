d7a9051bc7cfe7d83460bc31c693a6890386d19d
blyatsho <ma5740@fayoum.edu.eg>
2024-10-02 20:40:41 +0100

    Specifications

---
+++ Core/Domain/Contracts/Specifications.cs
@@ -0,0 +1,22 @@
+﻿
+global using System.Linq.Expressions;
+
+namespace Domain.Contracts
+{
+    public abstract class Specifications<T> where T : class
+    {
+        protected Specifications(Expression<Func<T, bool>>? criteria) 
+        {
+            Criteria = criteria;
+        }
+
+        public Expression<Func<T, bool>>? Criteria { get; }
+        public List<Expression<Func<T, object>>> IncludeExpression { get; } = new();
+        protected void AddInclude(Expression<Func<T, object>> expression)
+            => IncludeExpression.Add(expression);
+    }
+}
+// context.Set<T>().Where(EX).Include().OrdereBy()
+// Func<T, bool>
+// Where => Expression<Func<T, bool>>
+// Include => List<Expression<Func<T, object>>> 
---
+++ Core/Services/Specifications/ProductWithBrandAndTypeSpecifications.cs
@@ -0,0 +1,23 @@
+﻿
+using Domain.Contracts;
+
+namespace Services.Specifications
+{
+    internal class ProductWithBrandAndTypeSpecifications : Specifications<Product>
+    {
+        // Use to retrieve Product by Id
+        public ProductWithBrandAndTypeSpecifications(int id)
+            : base(product => product.Id == id)
+        {
+            AddInclude(product => product.ProductBrand);
+            AddInclude(product => product.ProductType);
+        }
+
+        public ProductWithBrandAndTypeSpecifications()
+            : base(null)
+        {
+            AddInclude(product => product.ProductBrand);
+            AddInclude(product => product.ProductType);
+        }
+    }
+}
