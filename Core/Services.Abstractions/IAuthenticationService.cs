c031284ef3c82b2291fb74c4c8d41ae6cb619eb4
blyatsho <ma5740@fayoum.edu.eg>
2024-10-23 23:42:09 +0100

    Authentication Service {Get & Update Address , Get user , Check Email}

--- Core/Services.Abstractions/IAuthenticationService.cs
+++ Core/Services.Abstractions/IAuthenticationService.cs
@@ -1,5 +1,6 @@
 ﻿
 using Shared.ErrorModels;
+using Shared.OrderModels;
 
 namespace Services.Abstractions
 {
@@ -8,5 +9,13 @@ namespace Services.Abstractions
         public Task<UserResultDTO> LoginAsync(LoginDTO loginModel);
         public Task<UserResultDTO> RegisterAsync(UserRegisterDTO registerModel);
 
+        // Get Current User
+        public Task<UserResultDTO> GetUserByEmail(string email);
+        // Check Email Exist
+        public Task<bool> CheckEmailExist(string email);
+        // Get User Address
+        public Task<AddressDTO> GetUserAddress(string email);
+        // Update User Address
+        public Task<AddressDTO> UpdateUserAddress(AddressDTO address, string email);
     }
 }
